67b80197d7ac03b0bb55106a2f96200da709d4a7
diligentyang <905407339@qq.com>
2017-05-23 20:11:43 +0800

    update memcache.php

--- Backend/lib/NoSql/Memcache.php
+++ Backend/lib/NoSql/Memcache.php
@@ -4,5 +4,33 @@ use \systems\DYBaseFunc as DYBaseFunc;
 
 class Memcache
 {
+	private static $_m = null;
+	private static $_cache = null;
+	
+	private function __construct($ip,$port){
+		self::$_m = new Memcache();
+		self::$_m->connect($ip,$port); 
+	}
+	
+	public static function getInstance($ip,$port){
+		if(!self::$_cache){
+            self::$_cache = new self($ip,$port);
+        }
+        return self::$_cache;
+	}
+	
+	private function __clone(){
+		DYBaseFunc::showErrors("Object cannot be cloned!");
+	}
+	
+	/**
+	* $key string 键
+	* $value string 值
+	* $time int 秒，有效期
+	*/
+	public function setItem($key,$value,$time = 300){
+		self::$_m->add($key,$value,$time);
+	}
+	
 	
 }
